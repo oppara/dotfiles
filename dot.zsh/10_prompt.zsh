bc83a29d02a378acc96caadf202dda1bb06e1708
oppara <oppara@oppara.tv>
2016-06-07 11:15:21 +0900

    Improve zsh prompt

--- dot.zsh/10_prompt.zsh
+++ dot.zsh/10_prompt.zsh
@@ -45,12 +45,12 @@ function get_vsc_info {
 
   # Check for untracked files
   if [ -n "$(git ls-files --others --exclude-standard)" ]; then
-      res="…"
+      res="♦︎ "
   fi
 
   # Check for uncommitted changes in the index
   if ! $(git diff --quiet --ignore-submodules --cached); then
-      res=""
+      res="…"
   fi
 
   # Check for unstaged changes
@@ -60,7 +60,7 @@ function get_vsc_info {
 
   # Check for stashed files
   if $(git rev-parse --verify refs/stash &>/dev/null); then
-      st="⚑"
+      st="⚑ "
   fi
 
   git_state=$res$st
