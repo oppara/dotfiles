ff63fb09ba3a0779947bc2ad62540417157ca111
oppara <oppara@oppara.tv>
2016-08-17 10:55:14 +0900

    Add ssh window name

--- dot.zsh/40_functions.zsh
+++ dot.zsh/40_functions.zsh
@@ -21,6 +21,21 @@ sudo() {
   esac
 }
 
+## ssh  #{{{1
+# http://qiita.com/shrkw/items/167be53796d4507c736b
+function ssh() {
+  if [[ -n $(printenv TMUX) ]]
+  then
+    local window_name=$(tmux display -p '#{window_name}')
+    tmux rename-window -- "$@[-1]" # zsh specified
+    # tmux rename-window -- "${!#}" # for bash
+    command ssh $@
+    tmux rename-window $window_name
+  else
+    command ssh $@
+  fi
+}
+
 ## gitignore #{{{1
 # http://gitignore.io/cli
 function gitignore() {
