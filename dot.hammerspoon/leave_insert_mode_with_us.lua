e670240a9ab83744d9f08293591215f0bd8aec5d
oppara <oppara@oppara.tv>
2017-02-02 22:47:37 +0900

    Improve hammerspoon for vim

--- dot.hammerspoon/leave_insert_mode_with_us.lua
+++ dot.hammerspoon/leave_insert_mode_with_us.lua
@@ -1,30 +1,50 @@
-local function keyCode(key, modifiers)
-   modifiers = modifiers or {}
-   return function()
-      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
-      hs.timer.usleep(1000)
-      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()
-   end
-end
+local VK_ESC = 53
+local VK_ESCAPE = 102
+local VK_LEFT_BRACKET = 33
+local VK_J = 38
 
 local function switchToUs()
     local cmd = '/usr/local/bin/keyboardSwitcher select U.S.'
     os.execute(cmd)
-    hs.console.printStyledtext(cmd)
+    -- hs.console.printStyledtext(cmd)
 end
 
-local ctrlBracket = hs.hotkey.bind({'ctrl'}, '[', keyCode('escape'), switchToUs)
-local ctrlJ = hs.hotkey.bind({'ctrl'}, 'j', keyCode('escape'), switchToUs)
+
+function flagsMatches(flags, modifiers)
+    local set = {}
+    for _, i in ipairs(modifiers) do set[string.lower(i)] = true end
+    for _, j in ipairs({'fn', 'cmd', 'ctrl', 'alt', 'shift'}) do
+        if set[j] ~= flags[j] then return false end
+    end
+    return true
+end
+
+
+keyEventtap = hs.eventtap.new({
+    hs.eventtap.event.types.keyDown
+}, function(event)
+    local keyCode = event:getKeyCode()
+    local flags = event:getFlags()
+    -- hs.console.printStyledtext(keyCode)
+
+    if keyCode == VK_ESC or keyCode == VK_ESCAPE then
+        switchToUs()
+    end
+
+    if keyCode == VK_LEFT_BRACKET or keyCode == VK_J then
+        if flagsMatches(flags, {'ctrl'}) then
+            switchToUs()
+        end
+    end
+end)
 
 local function handleGlobalEvent(name, event, app)
     if event == hs.application.watcher.activated then
         local bundleId = string.lower(app.frontmostApplication():bundleID())
         if bundleId == 'com.apple.terminal' then
-            ctrlBracket:enable()
-            ctrlJ:enable()
+            keyEventtap:start()
         else
-            ctrlBracket:disable()
-            ctrlJ:disable()
+            keyEventtap:stop()
         end
     end
 end
