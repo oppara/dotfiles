87668f9f7d9200c92836223d038e185916c590ed
oppara <oppara@oppara.tv>
2017-04-09 17:37:59 +0900

    Improve docker-machine setting

--- dot.zsh/99_misc.zsh
+++ dot.zsh/99_misc.zsh
@@ -11,8 +11,10 @@ test -d "${HOME}/.rbenv" &&  eval "$(rbenv init -)"
 ## plenv
 test -d "${HOME}/.plenv" &&  eval "$(plenv init -)"
 
-## docker-machine
-test -d "${HOME}/.docker/machine/machines/dev" && eval "$(docker-machine env dev)"
+## docker-machine dev
+if [[ $(docker-machine ls | grep dev | grep Running 2> /dev/null) ]]; then
+  eval "$(docker-machine env dev)"
+fi
 
 
 # vim: ft=sh fdm=marker
