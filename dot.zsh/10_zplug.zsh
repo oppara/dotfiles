02d1e68ded0dce4ce115de8d8bc5f663c627f465
oppara <oppara@oppara.tv>
2017-04-09 17:10:46 +0900

    Improve zplug shell script

--- dot.zsh/10_zplug.zsh
+++ dot.zsh/10_zplug.zsh
@@ -1,20 +1,21 @@
+export ZPLUG_HOME="/usr/local/opt/zplug"
 
-if [ -e /usr/local/opt/zplug ]; then
+if [ ! -e ${ZPLUG_HOME} ]; then
+    return 0
+fi
 
+source "${ZPLUG_HOME}/init.zsh"
 
-export ZPLUG_HOME=/usr/local/opt/zplug
-source $ZPLUG_HOME/init.zsh
-
-zplug 'zplug/zplug', hook-build:'zplug --self-manage'
+zplug "zplug/zplug", hook-build:"zplug --self-manage"
 
 zplug "mollifier/cd-gitroot"
-alias cgr='cd-gitroot'
+alias cgr="cd-gitroot"
 
 zplug "oppara/anyframe"
-bindkey '^r' anyframe-widget-put-history
-bindkey '^g^g' anyframe-widget-cd-ghq-repository
-bindkey '^s^s' anyframe-widget-insert-ssh-host
-bindkey '^g^i' anyframe-widget-insert-git-branch
+bindkey "^r" anyframe-widget-put-history
+bindkey "^g^g" anyframe-widget-cd-ghq-repository
+bindkey "^s^s" anyframe-widget-insert-ssh-host
+bindkey "^g^i" anyframe-widget-insert-git-branch
 alias gco=anyframe-widget-checkout-git-branch
 
 
@@ -29,5 +30,4 @@ fi
 zplug load
 
 
-fi
 
