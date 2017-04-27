ae7fb25a3faf59247ea4b61dce9c2da2f2a1a023
oppara <oppara@oppara.tv>
2017-04-27 13:00:43 +0900

    Remove sheet setting

--- dot.zsh/40_functions.zsh
+++ dot.zsh/40_functions.zsh
@@ -68,21 +68,6 @@ cdf() {
 }
 
 
-## sheet  #{{{1
-# http://blog.glidenote.com/blog/2012/04/16/sheet/
-compdef _sheets sheet
-function _sheets {
-  local -a cmds
-  _files -W  ~/.sheets/ -P '~/.sheets/'
-
-  cmds=('list' 'edit' 'copy')
-  _describe -t commands "subcommand" cmds
-
-  return 1;
-}
-
-
-
 ## checker for colors for .zshrc #{{{1
 function pcolor() {
   for ((f = 0; f < 255; f++)); do
