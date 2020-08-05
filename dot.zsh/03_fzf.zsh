03c2906e58c5f21d1723e9ac252d0dc222881fd4
oppara <oppara@oppara.tv>
2020-08-06 00:03:46 +0900

    Add fzf setting

---
+++ dot.zsh/03_fzf.zsh
@@ -0,0 +1,18 @@
+
+if which fzf >/dev/null; then
+    if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+        export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+    fi
+
+    # Auto-completion
+    [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+    # Key bindings
+    source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+
+    export FZF_DEFAULT_OPTS="--layout=reverse --inline-info"
+    if [[ -n $TMUX ]]; then
+        export FZF_TMUX=1
+    fi
+fi
+
