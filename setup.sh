94d545ae0f97930e26cb87ae4e7cc6d7e4553f89
oppara <oppara@oppara.tv>
2020-08-06 22:23:35 +0900

    format

--- setup.sh
+++ setup.sh
@@ -2,16 +2,16 @@
 
 set -eu
 
-create_symbolic_link () {
+create_symbolic_link() {
   local src="$1"
   local target="$2"
 
-  if test -e "$target" && ! test -L "$target" ; then
+  if test -e "$target" && ! test -L "$target"; then
     echo "file or directory already exists!! [${target}] "
     continue
   fi
 
-  if test -L "$target" ; then
+  if test -L "$target"; then
     rm "$target"
   fi
 
@@ -19,15 +19,13 @@ create_symbolic_link () {
   echo "ln -s $src $target"
 }
 
-
 CURRENT=$(cd $(dirname $0) && pwd)
 
-for file in $(ls "$CURRENT" | grep '^dot\.')
-do
+for file in $(ls "$CURRENT" | grep '^dot\.'); do
   org="$file"
 
   # osx用の設定
-  if echo "$file" | grep 'mac\.' > /dev/null; then
+  if echo "$file" | grep 'mac\.' >/dev/null; then
     if [ "${OSTYPE%%[^a-z]*}" != 'darwin' ]; then
       continue
     fi
@@ -41,8 +39,7 @@ do
   create_symbolic_link "$src" "$target"
 done
 
-for dir in $(ls "${CURRENT}/config")
-do
+for dir in $(ls "${CURRENT}/config"); do
   src="${CURRENT}/config/${dir}"
   target="${HOME}/.config/${dir}"
 
@@ -52,6 +49,5 @@ done
 if [ ! -e "${HOME}/.ssh/conf.d" ]; then
   mkdir -p "${HOME}"/.ssh/conf.d/{common,hosts}
 fi
-
 
 echo "done."
