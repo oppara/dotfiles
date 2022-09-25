039714fe38fe9ce5b4d634e357dbb1968b5da425
oppara <oppara@oppara.tv>
2022-09-25 09:57:39 +0900

    fix(shell): handle SC1091

--- dot.zsh/99_misc.zsh
+++ dot.zsh/99_misc.zsh
@@ -29,10 +29,11 @@ fi
 
 
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f "${HOME}/google-cloud-sdk/path.zsh.inc" ]; then . "${HOME}/google-cloud-sdk/path.zsh.inc"; fi
+test -f "${HOME}/google-cloud-sdk/path.zsh.inc" && "${HOME}/google-cloud-sdk/path.zsh.inc"
 
 # The next line enables shell command completion for gcloud.
-if [ -f "${HOME}/google-cloud-sdk/completion.zsh.inc" ]; then . "${HOME}/google-cloud-sdk/completion.zsh.inc"; fi
+test -f "${HOME}/google-cloud-sdk/completion.zsh.inc" && "${HOME}/google-cloud-sdk/completion.zsh.inc"
+
 
 if [ -L "${HOMEBREW_PREFIX}/bin/terraform" ]; then
   autoload -U +X bashcompinit && bashcompinit
