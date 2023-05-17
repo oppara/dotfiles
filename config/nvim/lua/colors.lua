a5a4be86661350d859db79f3ca694fe926949432
oppara <oppara@oppara.tv>
2023-05-18 07:01:52 +0900

    fix(neovim): use zenkau & zero-width space

--- config/nvim/lua/colors.lua
+++ config/nvim/lua/colors.lua
@@ -3,7 +3,7 @@ vim.api.nvim_create_augroup('full-width-whitespace', {})
 vim.api.nvim_create_autocmd({'VimEnter', 'WinEnter'}, {
     group = 'full-width-whitespace',
     pattern = {'*'},
-    command = [[call matchadd('FullWidthWhitespace', '[\u00A0\u2000-\u200B\u3000]')]]
+    command = [[call matchadd('FullWidthWhitespace', '[\u200B\u3000]')]]
 })
 vim.api.nvim_create_autocmd({'ColorScheme'}, {
     group = 'full-width-whitespace',
