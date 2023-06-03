12aad64db40073ccdb6dcc62ca84e19c83dfd395
oppara <oppara@oppara.tv>
2023-06-03 09:12:52 +0900

    fix(neovim): fix tagbar option

--- config/nvim/lua/plugins/tagbar.lua
+++ config/nvim/lua/plugins/tagbar.lua
@@ -3,9 +3,11 @@ vim.cmd([[
 
 noremap <silent> <leader>tl :TagbarOpenAutoClose<cr>
 
-let g:tagbar_left = 1
+let g:tagbar_position = 'topleft vertical'
+let g:tagbar_width = max([25, winwidth(0) / 5])
+
 let g:tagbar_sort = 0
-let g:tagbar_compact = 1
+let g:tagbar_compact = 2
 let g:tagbar_indent = 1
 let g:tagbar_iconchars = ['+', '-']
 
@@ -39,8 +41,4 @@ let g:tagbar_type_javascript = {
     \ ],
 \ }
 
-
 ]])
-
-
-
