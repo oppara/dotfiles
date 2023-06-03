e75fcc680e98d01427f41f780e0a76864687dc0f
oppara <oppara@oppara.tv>
2023-06-04 07:30:29 +0900

    feat(neovim): add tagbar typescript

--- config/nvim/lua/plugins/tagbar.lua
+++ config/nvim/lua/plugins/tagbar.lua
@@ -41,4 +41,18 @@ let g:tagbar_type_javascript = {
     \ ],
 \ }
 
+
+let g:tagbar_type_typescript = {
+  \ 'ctagstype': 'typescript',
+  \ 'kinds': [
+    \ 'c:classes',
+    \ 'n:modules',
+    \ 'f:functions',
+    \ 'v:variables',
+    \ 'v:varlambdas',
+    \ 'm:members',
+    \ 'i:interfaces',
+    \ 'e:enums',
+  \ ]
+\ }
 ]])
