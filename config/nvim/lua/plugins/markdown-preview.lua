7c2d297cac31308774b91829854e8107d85a4779
oppara <oppara@oppara.tv>
2023-06-05 10:28:46 +0900

    feat(neovim): user buffer local keymap

--- config/nvim/lua/plugins/markdown-preview.lua
+++ config/nvim/lua/plugins/markdown-preview.lua
@@ -1,6 +1,6 @@
 -- https://github.com/iamcco/markdown-preview.nvim
 
-local keymap = vim.api.nvim_set_keymap
+local keymap = vim.api.nvim_buf_set_keymap
 keymap('n', '<Leader>r', ':<C-u>MarkdownPreview<CR>', {noremap = true, silent = true})
 
 vim.g.mkdp_echo_preview_url = 1
