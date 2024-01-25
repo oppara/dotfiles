9f745a2fa53f14eace11c27b9f509e7828481b25
oppara <oppara@oppara.tv>
2024-01-25 12:30:42 +0900

    fix(neovim): update base-bash.sh

--- config/nvim/sonictemplate/sh/base-bash.sh
+++ config/nvim/sonictemplate/sh/base-bash.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-set -Ceu
+set -CEeuo pipefail
+# -C: リダイレクトで既存のファイルを上書きしない
+# -E: errtrace:エラーを継承
+# -e: errexit:エラーならシェル終了
+# -u: nounset:未設定変数はエラー
+# -o: オプションをonにする
+# pipefail: パイプライン途中のエラーを返り値にする
 
