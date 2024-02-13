bd1d7cace04ae32e38606aa675cb813d53cd410d
oppara <oppara@oppara.tv>
2024-02-13 17:22:48 +0900

    fix(neovim): fxi emmet settings

--- config/nvim/lua/plugins/emmet.lua
+++ config/nvim/lua/plugins/emmet.lua
@@ -5,7 +5,7 @@ let g:user_emmet_leader_key='<C-e>'
 
 let g:user_emmet_settings = {
       \  'variables': {
-      \    'lang': 'jaa',
+      \    'lang': 'ja',
       \    'charset': 'utf-8'
       \  },
       \ 'indentation': '  '
