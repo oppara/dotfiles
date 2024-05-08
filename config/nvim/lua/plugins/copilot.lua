c6ec0d260b464ebf55eff0d0056ef9ae7b516327
oppara <oppara@oppara.tv>
2024-05-08 23:04:36 +0900

    feat(neovim): add copilot#Accept to <CR> keymap

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -6,13 +6,6 @@ vim.g.copilot_filetypes = {
 }
 
 local keymap = vim.keymap.set
--- https://github.com/orgs/community/discussions/29817#discussioncomment-4217615
-keymap(
-  'i',
-  '<C-g>',
-  'copilot#Accept("<CR>")',
-  { silent = true, expr = true, script = true, replace_keycodes = false }
-)
 keymap('i', '<C-k>', '<Plug>(copilot-previous)')
 keymap('i', '<C-o>', '<Plug>(copilot-dismiss)')
 
