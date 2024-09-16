dc96f7ef58a65569ca534243db209a6a00135dc7
oppara <oppara@oppara.tv>
2024-09-16 19:27:35 +0900

    fix(nvim): fix #12 fix tagbar setting

--- config/nvim/lua/plugins/tagbar.lua
+++ config/nvim/lua/plugins/tagbar.lua
@@ -2,7 +2,7 @@
 vim.cmd([[
 
 noremap <silent> <leader>tl :TagbarOpenAutoClose<cr>
-
+"
 let g:tagbar_position = 'topleft vertical'
 let g:tagbar_width = max([25, winwidth(0) / 5])
 
@@ -10,49 +10,4 @@ let g:tagbar_sort = 0
 let g:tagbar_compact = 2
 let g:tagbar_indent = 1
 let g:tagbar_iconchars = ['+', '-']
-
-" https://github.com/majutsushi/tagbar/wiki
-let g:tagbar_type_markdown = {
-    \ 'ctagstype' : 'markdown',
-    \ 'kinds' : [
-        \ 'h:H1',
-        \ 'i:H2',
-        \ 'k:H3',
-        \ 'l:H4'
-    \ ]
-\ }
-
-let g:tagbar_type_php = {
-    \ 'kinds' : [
-        \ 'i:interfaces',
-        \ 'c:classes',
-        \ 'd:constant definitions:1:0',
-        \ 'f:functions',
-    \ ],
-\ }
-
-let g:tagbar_type_javascript = {
-    \ 'kinds' : [
-        \ 'I:interfaces',
-        \ 'C:classes',
-        \ 'D:constant definitions:1:0',
-        \ 'F:functions',
-        \ 'V:variables',
-    \ ],
-\ }
-
-
-let g:tagbar_type_typescript = {
-  \ 'ctagstype': 'typescript',
-  \ 'kinds': [
-    \ 'c:classes',
-    \ 'n:modules',
-    \ 'f:functions',
-    \ 'v:variables',
-    \ 'v:varlambdas',
-    \ 'm:members',
-    \ 'i:interfaces',
-    \ 'e:enums',
-  \ ]
-\ }
 ]])
