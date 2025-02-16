b562323bbc4fb7101564185e6f57e1ca6642d137
oppara <oppara@oppara.tv>
2025-02-16 11:05:09 +0900

    config(nvim): implove markdown settings

--- config/nvim/ftplugin/markdown.lua
+++ config/nvim/ftplugin/markdown.lua
@@ -1,10 +1,13 @@
 vim.cmd([[
     set commentstring=<\!--\ %s\ -->
+
+    augroup vimrc-ft-markdown
+      autocmd!
+      autocmd FileType markdown setlocal expandtab softtabstop=2 shiftwidth=2
+      " surround.vim
+      autocmd FileType markdown let b:surround_{char2nr('l')} = "[](\r)"
+    augroup END
+
 ]])
 
 
--- augroup vimrc-ft-markdown
---   autocmd!
---   " surround.vim
---   autocmd FileType markdown let b:surround_{char2nr('l')} = "[](\r)"
--- augroup END
