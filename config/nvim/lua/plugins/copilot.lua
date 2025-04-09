1ad5bf067691446fd81cf0fd53ad0f33de7a82d7
oppara <oppara@oppara.tv>
2025-04-09 23:12:47 +0900

    config(nvim): set copilot mapping

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -1,3 +1,4 @@
+vim.g.copilot_no_maps = true
 
 vim.g.copilot_filetypes = {
   markdown = true;
@@ -8,3 +9,10 @@ local keyset = vim.keymap.set
 keyset('i', '<C-k>', '<Plug>(copilot-previous)')
 keyset('i', '<C-o>', '<Plug>(copilot-dismiss)')
 
+
+keyset(
+'i',
+'<C-g>',
+'copilot#Accept("<CR>")',
+{ silent = true, expr = true, script = true, replace_keycodes = false }
+)
