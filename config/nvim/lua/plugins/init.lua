c97cfb94526597f3dda1b039caac2abd3bef1357
oppara <oppara@oppara.tv>
2025-07-17 00:46:19 +0900

    perf(nvim): Improve copilot-cmp setup

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -99,7 +99,10 @@ require("jetpack.packer").add({
         "zbirenbaum/copilot-cmp",
         after = { "copilot.lua", "nvim-cmp" },
         config = function()
-            require("copilot_cmp").setup()
+            require("copilot_cmp").setup({
+                event = { "InsertEnter", "LspAttach" },
+                fix_pairs = true,
+            })
         end,
     },
     -- {
