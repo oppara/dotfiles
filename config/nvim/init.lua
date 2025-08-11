bf7b68a610c08d4b0c2d1c327424f6a378eb4375
oppara <oppara@oppara.tv>
2025-08-11 15:32:31 +0900

    style(nvim): format

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,13 +1,13 @@
 -- cache init.lua
 vim.loader.enable()
 
-vim.scriptencoding = 'utf-8'
-vim.language = 'C'
+vim.scriptencoding = "utf-8"
+vim.language = "C"
 
 --  global
-vim.g.oppara_email = 'oppara <oppara _at_ oppara.tv>'
+vim.g.oppara_email = "oppara <oppara _at_ oppara.tv>"
 vim.g.snips_author = vim.g.oppara_email
-vim.g.changelog_username = 'oppara'
+vim.g.changelog_username = "oppara"
 
 -- matchparen をやめる
 vim.g.loaded_matchparen = 1
@@ -15,7 +15,6 @@ vim.g.loaded_matchparen = 1
 -- 改行コードが LF に固定されるのを回避
 -- https://zenn.dev/oppara/scraps/1442ab8273f73f
 vim.g.editorconfig = false
-
 
 vim.cmd([[
 
@@ -35,13 +34,12 @@ _G.P = function(value)
 end
 
 -- require
-require('encodings')
-require('options')
-require('plugins')
-require('colors')
-require('mappings')
-require('commands')
-require('autocmds')
-require('filetypes')
-require('lsp')
-
+require("encodings")
+require("options")
+require("plugins")
+require("colors")
+require("mappings")
+require("commands")
+require("autocmds")
+require("filetypes")
+require("lsp")
