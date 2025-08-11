30a2da9dcd865ebbd7f6c59c4455c7aa8ef4c27b
oppara <oppara@oppara.tv>
2025-08-11 15:30:07 +0900

    style(nvim): fix indent

--- config/nvim/lua/utils.lua
+++ config/nvim/lua/utils.lua
@@ -1,37 +1,38 @@
 local M = {}
 
 function M.dump(o)
-  if type(o) == 'table' then
-    local s = '{ '
-    for k,v in pairs(o) do
-      if type(k) ~= 'number' then k = '"'..k..'"' end
-      s = s .. '['..k..'] = ' .. dump(v) .. ','
+    if type(o) == "table" then
+        local s = "{ "
+        for k, v in pairs(o) do
+            if type(k) ~= "number" then
+                k = '"' .. k .. '"'
+            end
+            s = s .. "[" .. k .. "] = " .. dump(v) .. ","
+        end
+        return s .. "} "
+    else
+        return tostring(o)
     end
-    return s .. '} '
-  else
-    return tostring(o)
-  end
 end
 
 M.usePlugin = function(name)
-  local status, plugin = pcall(require, name)
-  if not status then
-    return false
-  end
-  return plugin
+    local status, plugin = pcall(require, name)
+    if not status then
+        return false
+    end
+    return plugin
 end
 
-
 M.setIndent = function(tabLength, useHardTab)
-  if useHardTab then
-    vim.bo.expandtab = false
-  else
-    vim.bo.expandtab = true
-  end
+    if useHardTab then
+        vim.bo.expandtab = false
+    else
+        vim.bo.expandtab = true
+    end
 
-  vim.bo.shiftwidth  = tabLength
-  vim.bo.softtabstop = tabLength
-  vim.bo.tabstop     = tabLength
+    vim.bo.shiftwidth = tabLength
+    vim.bo.softtabstop = tabLength
+    vim.bo.tabstop = tabLength
 end
 
 return M
