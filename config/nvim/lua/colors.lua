cc5a1647cd96e8840311b1c777c5231dd7dc627d
oppara <oppara@oppara.tv>
2025-08-27 01:18:00 +0900

    style(nvim): format

--- config/nvim/lua/colors.lua
+++ config/nvim/lua/colors.lua
@@ -1,16 +1,14 @@
-
 vim.api.nvim_create_augroup('full-width-whitespace', {})
-vim.api.nvim_create_autocmd({'VimEnter', 'WinEnter'}, {
-    group = 'full-width-whitespace',
-    pattern = {'*'},
-    command = [[call matchadd('FullWidthWhitespace', '[\u200B\u3000]')]]
+vim.api.nvim_create_autocmd({ 'VimEnter', 'WinEnter' }, {
+  group = 'full-width-whitespace',
+  pattern = { '*' },
+  command = [[call matchadd('FullWidthWhitespace', '[\u200B\u3000]')]],
 })
-vim.api.nvim_create_autocmd({'ColorScheme'}, {
-    group = 'full-width-whitespace',
-    pattern = {'*'},
-    command = [[highlight default FullWidthWhitespace ctermbg=202 ctermfg=202 guibg=salmon]]
+vim.api.nvim_create_autocmd({ 'ColorScheme' }, {
+  group = 'full-width-whitespace',
+  pattern = { '*' },
+  command = [[highlight default FullWidthWhitespace ctermbg=202 ctermfg=202 guibg=salmon]],
 })
 
 vim.cmd('colorscheme kanagawa')
 vim.cmd('colorscheme wombat_classic')
-
