4489cf75a1efb60e868087ae890108c15eb5a633
oppara <oppara@oppara.tv>
2025-09-01 23:32:58 +0900

    feat(nvim): setup lualine with everforest theme

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -1,95 +1,11 @@
--- https://github.com/nvim-lualine/lualine.nvim/blob/master/lua/lualine/themes/wombat.lua
-local colors = {
-  base02 = '#444444',
-  base3 = '#d0d0d0',
-  red = '#e5786d',
-  black = '#131313',
-  green = '#95e454',
-  blue = '#8ac6f2',
-}
+--
+-- https://github.com/nvim-lualine/lualine.nvim
+--
 
 require('lualine').setup({
   options = {
     theme = 'everforest',
     icons_enabled = false,
-    component_separators = { left = '|', right = '|' },
-    section_separators = {},
-    disabled_filetypes = {},
-  },
-  sections = {
-    lualine_a = {
-      {
-        '%h + ',
-        cond = function()
-          return vim.bo.modified
-        end,
-        component_separators = {},
-        color = { fg = colors.base02, bg = '#dddddd', gui = 'bold' },
-        padding = 0,
-      },
-      {
-        '%h N ',
-        cond = function()
-          local filename = vim.fn.expand('%')
-          return filename ~= '' and vim.bo.buftype == '' and vim.fn.filereadable(filename) == 0
-        end,
-        component_separators = {},
-        color = { fg = colors.base02, bg = colors.blue, gui = 'bold' },
-        padding = 0,
-      },
-      {
-        '%h RO ',
-        cond = function()
-          return vim.bo.readonly
-        end,
-        component_separators = {},
-        color = { fg = colors.base02, bg = colors.red, gui = 'bold' },
-        padding = 0,
-      },
-    },
-    lualine_b = {
-      {
-        'filename',
-        color = { fg = colors.base3, bg = colors.base02 },
-        file_status = false,
-        newfile_status = true,
-        path = 1,
-        shorting_target = 40,
-      },
-      {
-        'diff',
-        color = { bg = colors.base02 },
-      },
-    },
-    lualine_c = {
-      {
-        'diagnostics',
-        sources = { 'nvim_diagnostic', 'coc', 'ale' },
-        symbols = { error = 'E:', warn = 'W:', info = 'I:', hint = 'H:' },
-      },
-    },
-    lualine_x = { 'branch', 'encoding', 'fileformat', 'filetype' },
-    lualine_y = {
-      {
-        'progress',
-        color = { fg = colors.black },
-      },
-      {
-        'location',
-        color = { fg = colors.black },
-        padding = 0,
-      },
-    },
-    lualine_z = {},
-  },
-  inactive_sections = {
-    lualine_c = {
-      {
-        'filename',
-        path = 3,
-      },
-    },
-    lualine_x = { 'location' },
   },
   extensions = { 'quickfix' },
 })
