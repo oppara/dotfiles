47a022111f449116f8007d4dce7239854f0efdf3
oppara <oppara@oppara.tv>
2025-09-06 14:34:10 +0900

    fix(nvim): fix lua_ls workspace error

--- config/nvim/after/lsp/lua_ls.lua
+++ config/nvim/after/lsp/lua_ls.lua
@@ -1,6 +1,16 @@
 return {
   settings = {
     Lua = {
+      workspace = {
+        checkThirdParty = false,
+        ignoreDir = {
+          '.git',
+          'node_modules',
+          '.vscode',
+        },
+        maxPreload = 1000,
+        preloadFileSize = 10000,
+      },
       diagnostics = {
         globals = {
           'vim',
