249c290cf16f6584d2f7942eccf795dd0c8a9512
oppara <oppara@oppara.tv>
2025-09-07 08:10:36 +0900

    fix(nvim):  fix lualine mode width

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -7,5 +7,16 @@ require('lualine').setup({
     theme = 'everforest',
     icons_enabled = false,
   },
+  sections = {
+    lualine_a = {
+      {
+        'mode',
+        fmt = function(str, _)
+          local max_len = 7
+          return str .. string.rep(' ', max_len - #str)
+        end,
+      },
+    },
+  },
   extensions = { 'quickfix' },
 })
