40c521c8d439efff4c09020befed2356d887266a
oppara <oppara@oppara.tv>
2025-09-07 08:32:45 +0900

    feat(nvim): improve lualine file status display

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -1,6 +1,21 @@
 --
 -- https://github.com/nvim-lualine/lualine.nvim
 --
+--
+
+local function file_symbols()
+  if vim.bo.readonly then
+    return 'RO'
+  elseif vim.bo.modified then
+    return '+'
+  elseif vim.fn.bufname() == '' then
+    return 'No Name'
+  elseif vim.bo.buftype == '' and vim.fn.filereadable(vim.fn.expand('%:p')) == 0 then
+    return 'New'
+  else
+    return ''
+  end
+end
 
 require('lualine').setup({
   options = {
@@ -10,11 +25,20 @@ require('lualine').setup({
   sections = {
     lualine_a = {
       {
+        file_symbols,
+      },
+      {
         'mode',
         fmt = function(str, _)
           local max_len = 7
           return str .. string.rep(' ', max_len - #str)
         end,
+      },
+    },
+    lualine_c = {
+      {
+        'filename',
+        file_status = false,
       },
     },
   },
