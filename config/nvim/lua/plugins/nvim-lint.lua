6eaa14c86aead10026ba45e237a5c70659132571
oppara <oppara@oppara.tv>
2025-09-28 12:47:33 +0900

    fead(nvim): add textlint linter

--- config/nvim/lua/plugins/nvim-lint.lua
+++ config/nvim/lua/plugins/nvim-lint.lua
@@ -1,8 +1,49 @@
 local lint = require('lint')
 
 local severities = {
-  warning = vim.diagnostic.severity.WARN,
-  error = vim.diagnostic.severity.ERROR,
+  [0] = vim.diagnostic.severity.INFO,
+  [1] = vim.diagnostic.severity.WARN,
+  [2] = vim.diagnostic.severity.ERROR,
+}
+
+lint.linters.textlint = {
+  cmd = 'textlint',
+  args = {
+    '--format',
+    'json',
+    '--stdin',
+    '--stdin-filename',
+    function()
+      return vim.api.nvim_buf_get_name(0)
+    end,
+  },
+  stdin = true,
+  stream = 'stdout',
+  ignore_exitcode = true,
+  parser = function(output)
+    if output == '' then
+      return {}
+    end
+    local decoded = vim.json.decode(output)
+    if decoded == nil then
+      return {}
+    end
+    local diagnostics = {}
+    for _, value in ipairs(decoded) do
+      for _, item in ipairs(value.messages) do
+        table.insert(diagnostics, {
+          message = item.message,
+          col = item.loc.start.column - 1,
+          end_col = item.loc['end'].column - 1,
+          lnum = item.loc.start.line - 1,
+          end_lnum = item.loc['end'].line - 1,
+          severity = severities[item.severity],
+          source = 'textlint',
+        })
+      end
+    end
+    return diagnostics
+  end,
 }
 
 local stylelint = lint.linters.stylelint
@@ -47,7 +88,7 @@ lint.linters_by_ft = {
   scss = { 'stylelint' },
   sass = { 'stylelint' },
   less = { 'stylelint' },
-  markdown = { 'markdownlint-cli2' },
+  markdown = { 'markdownlint-cli2', 'textlint' },
   python = { 'ruff', 'flake8' },
   -- brew install sqruff
   sql = { 'sqruff' },
