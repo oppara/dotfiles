615ed55f8a46b737bb118c3c1df950993b3c5aa7
oppara <oppara@oppara.tv>
2026-05-15 19:18:27 +0900

    Revert "feat(nvim): use telescope for lsp navigation"
    This reverts commit 7f85faeb832981683b108d158f9a72b2805d83f3.

--- dot.config/nvim/lua/plugins/telescope-nvim.lua
+++ dot.config/nvim/lua/plugins/telescope-nvim.lua
@@ -26,13 +26,3 @@ vim.keymap.set('n', 'fff', builtin.find_files, { desc = 'Telescope find files' }
 vim.keymap.set('n', 'ffg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', 'ffb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', 'ffo', builtin.oldfiles, { desc = 'Telescope oldfiles' })
-
-vim.keymap.set('n', 'gd', builtin.lsp_definitions)
-vim.keymap.set('n', 'gD', builtin.lsp_type_definitions)
-vim.keymap.set('n', 'gi', builtin.lsp_implementations)
-vim.keymap.set('n', 'gr', function()
-  require('telescope.builtin').lsp_references({
-    show_line = false,
-    include_declaration = false,
-  })
-end)
