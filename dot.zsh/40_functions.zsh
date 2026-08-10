0d613da441f4c05c837ec0da189b69c0ed3ef355
oppara <oppara@oppara.tv>
2026-08-11 00:07:52 +0900

    fix(tmux): make ssh window renaming actually take effect

--- dot.zsh/40_functions.zsh
+++ dot.zsh/40_functions.zsh
@@ -22,15 +22,15 @@ sudo() {
 
 ## ssh  #{{{1
 # http://qiita.com/shrkw/items/167be53796d4507c736b
+unalias ssh 2>/dev/null
 function ssh() {
   if [[ -n $(printenv TMUX) ]]; then
-    local window_name=$(tmux display -p '#{window_name}')
     tmux rename-window -- "$@[-1]" # zsh specified
     # tmux rename-window -- "${!#}" # for bash
-    command ssh $@
-    tmux rename-window $window_name
+    env TERM=xterm-256color command ssh $@
+    tmux set-window-option automatic-rename on
   else
-    command ssh $@
+    env TERM=xterm-256color command ssh $@
   fi
 }
 
