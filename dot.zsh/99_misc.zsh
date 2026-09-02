59397c59042bd3c9286c1cdf3be0db046b35230f
oppara <oppara@oppara.tv>
2026-09-03 07:27:21 +0900

    feat(zsh): add tirith init
    - ターミナルセキュリティツール tirith の shell hook を追加

--- dot.zsh/99_misc.zsh
+++ dot.zsh/99_misc.zsh
@@ -65,4 +65,10 @@ if command -v oh-my-posh &>/dev/null; then
   eval "$(oh-my-posh init zsh --config  $XDG_CONFIG_HOME/oh-my-posh/opp.omp.toml)"
 fi
 
+# tirith
+# https://github.com/sheeki03/tirith
+if command -v tirith &>/dev/null; then
+  eval "$(tirith init --shell zsh)"
+fi
+
 # vim: ft=zsh fdm=marker
